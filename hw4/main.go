7c38decfcc892f959256e3ccf8428d735e5355db
shodhub <shodvaly@gmail.com>
2024-07-17 17:43:19 +0500

    hw4 done

--- hw4/main.go
+++ hw4/main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello")
+	PrintGreeting()
+}
+// Создайте функцию PrintGreeting, которая печатает "Доброе утро!", если dayType равен "утро";
+//  "Добрый день!", если dayType равен "день"; и "Добрый вечер!", если dayType равен "вечер". 
+//  Переменную  dayType вводить с консоли внутри функции.
+func PrintGreeting(){
+	var dayType string
+	fmt.Scanln(&dayType)
+	if dayType=="утро"{
+		fmt.Println("Доброе утро!")
+	}
+	if dayType=="день"{
+		fmt.Println("Добрый день!")
+	}
+	if dayType=="вечер"{
+		fmt.Println("Добрый вечер!")
+	}
 }
